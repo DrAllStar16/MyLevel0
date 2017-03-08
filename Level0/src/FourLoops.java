66d536f9e6e3e28f92bd66503ef4caba2e550fcd
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-03-07 16:58:38 -0800

    Fourlopps

--- Level0/src/FourLoops.java
+++ Level0/src/FourLoops.java
@@ -1,39 +1,43 @@
+import javax.swing.JOptionPane;
+
 import com.spun.util.database.SQLQuery.FromPart;
 
 public class FourLoops {
-	
-	
+
 	public static void main(String[] args) {
-		
-		  for (int a = 0; a > -100; a++) {
-				System.out.print ( a+ " " );
-			}
-		  
-		  for (int i = 100; i > -1; i--) {
-			System.out.print ( i+ " " );
+
+		for (int a = 0; a > -100; a++) {
+			System.out.print(a + " ");
 		}
-	
-	
-	for (int n = 2; n < 102; n+=2) {
-		System.out.print (n+ " ");
-	}
-	
-	for (int t = 1; t < 101; t+=2) {
-		System.out.print (t+ " ");
-	}
-	
-		
+
+		for (int i = 100; i > -1; i--) {
+			System.out.print(i + " ");
+		}
+
+		for (int n = 2; n < 102; n += 2) {
+			System.out.print(n + " ");
+		}
+
+		for (int t = 1; t < 101; t += 2) {
+			System.out.print(t + " ");
+		}
+
 		for (int w = 0; w < 501; w++) {
-			
-if (w%2==1) {
-	System.out.print(w+ " odd ");
-}			
-else if (w%2==0) {
-	System.out.print(w+ " even ");
-}			
+
+			if (w % 2 == 1) {
+				System.out.print(w + " odd ");
+			} else if (w % 2 == 0) {
+				System.out.print(w + " even ");
+			}
 		}
-		
-		
+
+		for (int e = 0; e < 111; e++) {
+
+			if (e % 7 == 7) {
+				System.out.println(e);
+			}
+			
+		}
 	}
-	
+
 }
