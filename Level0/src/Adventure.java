548c11ca788fe0d87e81de05e097d455f27a9947
DrAllStar16 <league@WTS-iMac-11.attlocal.net>
2017-04-03 19:48:55 -0700

    Adventure time 2

--- Level0/src/Adventure.java
+++ Level0/src/Adventure.java
@@ -1,17 +1,68 @@
 import javax.swing.JOptionPane;
 
 public class Adventure {
-public static void main(String[] args) {
-	
-	String Num1=JOptionPane.showInputDialog("You wake up on a Sunday at 7:45 do you want >to get up< or >stay in bed<");
-	
-	if (Num1.equals("stay in bed") ) {
-		String Num21= JOptionPane.showInputDialog("You went back to sleep and it is now 8:30 do you want to >stay in bed< or >get up<");
+	public static void main(String[] args) {
+
+		String Num1 = JOptionPane.showInputDialog("you could get a >tablet< or a >phone<");
+		// -------------------------------------------------------------------------------------------------------------------------------------
+		if (Num1.equals("tablet")) {
+			Num1 = JOptionPane.showInputDialog(">Apple1< or >Andrio1d<");
+		}
+
+		if (Num1.equals("phone")) {
+			Num1 = JOptionPane.showInputDialog(">Apple2< or >Andriod2<");
+		}
+
+		// ______________________________________________________________________________________________________________________________________
+
+		if (Num1.equals("Apple1")) {
+			Num1 = JOptionPane.showInputDialog("Whould you get the >SE< or the >6<");
+		}
+
+		if (Num1.equals("Andriod1")) {
+			Num1 = JOptionPane.showInputDialog("Would you get the >S5< or >Moto Mod Driod<");
+		}
+
+		if (Num1.equals("Apple2")) {
+			Num1 = JOptionPane.showInputDialog("Would you get the >I pad< or the >I pad Mini<");
+		}
+
+		if (Num1.equals("Andriod2")) {
+			Num1 = JOptionPane.showInputDialog("Woudl you get the >Galaxy Tab< or the >Galaxy tab A<");
+		}
+
+		// ==================================================================================================================================
+
+		if (Num1.equals("SE")) {
+			Num1 = JOptionPane.showInputDialog("Nice Choice");
+
+			if (Num1.equals("6")) {
+				Num1 = JOptionPane.showInputDialog("Nice Choice");
+			}
+
+			if (Num1.equals("S5")) {
+				Num1 = JOptionPane.showInputDialog("Nice Choice");
+			}
+
+			if (Num1.equals("Moto Mod Driod")) {
+				Num1 = JOptionPane.showInputDialog("Nice Choice");
+			}
+
+			if (Num1.equals("I pad")) {
+				Num1 = JOptionPane.showInputDialog("Nice Choice");
+			}
+
+			if (Num1.equals("I pad Mini")) {
+				Num1 = JOptionPane.showInputDialog("Nice Choice");
+			}
+
+			if (Num1.equals("Galaxy Tab")) {
+				Num1 = JOptionPane.showInputDialog("Nice Choice");
+			}
+
+			if (Num1.equals("Galaxy Tab A")) {
+				Num1 = JOptionPane.showInputDialog("Nice Choice");
+			}
+		}
 	}
-	
-	if (Num1.equals("get up") ) {
-		String Num22= JOptionPane.showInputDialog("You are now up do you want to go to >Dennys< or >Dount Touch<'");
-	}
-	
-}
 }
